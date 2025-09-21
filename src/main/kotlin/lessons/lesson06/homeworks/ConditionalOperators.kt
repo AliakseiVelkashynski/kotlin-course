d86afcd67f73815f56005777f0e1aa72b730b42c
velaleksei <avelkoshinskiy@alfabank.ru>
2025-09-21 12:54:07 +0300

    added printTransportAdvice в ConditionalOperators.kt

--- src/main/kotlin/lessons/lesson06/homeworks/ConditionalOperators.kt
+++ src/main/kotlin/lessons/lesson06/homeworks/ConditionalOperators.kt
@@ -1,27 +1,34 @@
 package org.example.lessons.lesson06.homeworks
 
-    fun printSeason(month: Int) {
-        when (month) {
-            1, 2, 12 -> println("Зима")
-            in 3..5 -> println("Весна")
-            in 6..8 -> println("Лето")
-            in 9..11 -> println("Осень")
-            else -> println("Некорректный номер месяца")
-        }
+fun printSeason(month: Int) {
+    when (month) {
+        1, 2, 12 -> println("Зима")
+        in 3..5 -> println("Весна")
+        in 6..8 -> println("Лето")
+        in 9..11 -> println("Осень")
+        else -> println("Некорректный номер месяца")
     }
+}
 
-    fun convertDogAgeToHumanAge(dogAge: Int) {
-        val humanAge = when {
-            dogAge < 1 -> 0.0
-            dogAge <= 2 -> dogAge * 10.5
-            else -> 21 + (dogAge - 2) * 4.0
-        }
-        println("Человеческий возраст $humanAge лет")
+fun convertDogAgeToHumanAge(dogAge: Int) {
+    val humanAge = when {
+        dogAge < 1 -> 0.0
+        dogAge <= 2 -> dogAge * 10.5
+        else -> 21 + (dogAge - 2) * 4.0
     }
+    println("Человеческий возраст $humanAge лет")
+}
 
-
-
-
-
-    fun main() {
+fun printTransportAdvice(distance: Double) {
+    val advice = when {
+        distance <= 1 -> "пешком"
+        distance <= 5 -> "велосипед"
+        else -> "автотранспорт"
     }
+    println("Для маршрута длиной $distance км рекомендуется: $advice")
+}
+
+
+fun main() {
+    printTransportAdvice(5.6)
+}
