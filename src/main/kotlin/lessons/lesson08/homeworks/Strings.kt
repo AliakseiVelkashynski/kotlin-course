c2a686569e9c0d0bff4366f2138c2eea7d904f01
velaleksei <avelkoshinskiy@alfabank.ru>
2025-10-02 13:55:46 +0300

    fixes in Strings.kt

--- src/main/kotlin/lessons/lesson08/homeworks/Strings.kt
+++ src/main/kotlin/lessons/lesson08/homeworks/Strings.kt
@@ -3,23 +3,18 @@ package org.example.lessons.lesson08.homeworks
 fun makeFunky(text: String) {
     var result = text
 
-    // 1. Замена "невозможно"
     result = result.replace("невозможно", "совершенно точно возможно, просто требует времени")
 
-    // 2. Добавление при начале с "Я не уверен"
     if (result.startsWith("Я не уверен")) {
         result += ", но моя интуиция говорит об обратном"
     }
 
-    // 3. Замена "катастрофа"
     result = result.replace("катастрофа", "интересное событие")
 
-    // 4. Замена окончания "без проблем"
     if (result.endsWith("без проблем")) {
         result = result.replace("без проблем", "с парой интересных вызовов на пути")
     }
 
-    // 5. Односложночные фразы
     val words = result.split(" ")
     if (words.size == 1) {
         result = "Иногда, $result, но не всегда"
@@ -32,13 +27,10 @@ fun getDateAndTime(logInfo: String) {
 
     val log = "Пользователь вошел в систему -> 2021-12-01 09:48:23"
 
-    // 1. Разделяем строку по "->" и берем вторую часть
     val dateTimePart = log.split("->")[1].trim()
 
-    // 2. Разделяем дату и время по пробелу
     val (date, time) = dateTimePart.split(" ")
 
-    // 3. Выводим результат
     println("Дата: $date")
     println("Время: $time")
 
@@ -62,7 +54,7 @@ fun maskCreditCard(number: String) {
         result += maskedPart
 
         if (i < parts.size - 1) {
-            result += " " // Добавляем разделитель между частями
+            result += " "
         }
     }
 
