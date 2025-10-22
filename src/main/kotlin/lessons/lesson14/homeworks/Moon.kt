bed3d0619dc403b349f6af8eca2cb8345e4efff8
velaleksei <avelkoshinskiy@alfabank.ru>
2025-10-22 20:58:10 +0300

    added Moon class

---
+++ src/main/kotlin/lessons/lesson14/homeworks/Moon.kt
@@ -0,0 +1,18 @@
+package org.example.lessons.lesson14.homeworks
+
+object Moon {
+    val isVisible: Boolean = true
+    val phase: String = "New Moon"
+    fun showPhase() {
+        if (isVisible) {
+            println("Луна видна и находится в фазе $phase.")
+        } else {
+            println("Луна не видна")
+        }
+    }
+}
+
+
+fun main() {
+    Moon.showPhase()
+}
